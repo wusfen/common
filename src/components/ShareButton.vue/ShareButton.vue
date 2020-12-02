643a45df6be8e55c942f148b88d282f28224b65b
wushufen <wusfun@foxmail.com>
2020-12-02 11:02:34 +0800

    + ShareButton.vue

---
+++ src/components/ShareButton.vue/ShareButton.vue
@@ -0,0 +1,46 @@
+<template>
+  <button @click="isShowShareTips = true">
+    <slot>分享</slot>
+    <!-- move to body -->
+    <Overlay ref="overlay" class="shareTips" v-model="isShowShareTips">
+      <img src="./img/share-tips.png" alt="请点击右上角分享" />
+    </Overlay>
+  </button>
+</template>
+
+<script>
+import Overlay from '@common/components/Overlay'
+
+export default {
+  props: {},
+  components: {
+    Overlay,
+  },
+  data() {
+    return {
+      isShowShareTips: false,
+    }
+  },
+  computed: {},
+  async mounted() {
+    // move to body
+    var overlay = this.$refs.overlay
+    document.body.appendChild(overlay.$el)
+  },
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+.shareTips {
+  padding: 1em;
+  background: rgba(0, 0, 0, 0.5);
+  display: block;
+  text-align: right;
+  img {
+    pointer-events: none;
+    width: 560px;
+    object-fit: contain;
+  }
+}
+</style>
