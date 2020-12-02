b5d0368058e062f9931db579065b8c92b676c6c9
wushufen <wusfun@foxmail.com>
2020-12-02 10:59:50 +0800

    onlongpress bubbles

--- src/utils/onlongpress.js
+++ src/utils/onlongpress.js
@@ -8,24 +8,17 @@ var touchstartTimer
 addEventListener('touchstart', e => {
   moveCount = 0
   touchstartTimer = setTimeout(() => {
-    !(function loopUp(el) {
-      if (!el || !el.tagName) return
-
-      // @longpress
-      el.dispatchEvent(new Event('longpress'))
-      el.dispatchEvent(new Event('longPress'))
-
-      return loopUp(el.parentNode)
-    })(e.target)
-  }, 1000)
+    e.target.dispatchEvent(new Event('longpress', { bubbles: true }))
+  }, 500)
 })
 addEventListener('touchmove', e => {
-  if (++moveCount > 10) {
+  if (++moveCount > 5) {
     clearTimeout(touchstartTimer)
   }
 })
+
+// 安卓长按图片弹窗菜单后不会触发 touchend
+// 所以必须用 setTimeout 派发事件
 addEventListener('touchend', e => {
-  // 安卓长按图片弹窗菜单后不会触发 touchend
-  // 所以用 setTimeout 来触发
   clearTimeout(touchstartTimer)
 })
