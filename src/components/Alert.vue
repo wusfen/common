279ad32f874d0a4bf43ef40a4b85a9e92f438cbc
wushufen <wusfun@foxmail.com>
2020-12-25 18:00:22 +0800

    Alert.vue

---
+++ src/components/Alert.vue
@@ -0,0 +1,51 @@
+<template>
+  <Modal v-if="title || message" v-model="isShow" @close="cancel">
+    <template slot="title">
+      {{ title }}
+    </template>
+    <div class="message">
+      {{ message }}
+    </div>
+    <template slot="footer">
+      <button class="primary" @click="ok">确认</button>
+      <!-- <button @click="cancel">取消</button> -->
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from './Modal'
+
+export default {
+  components: { Modal },
+  props: {
+    title: String,
+    message: String,
+  },
+  data() {
+    return {
+      isShow: true,
+    }
+  },
+  computed: {},
+  async mounted() {},
+  methods: {
+    ok() {
+      this.isShow = false
+      this.$emit('ok')
+      this.$emit('close')
+    },
+    cancel() {
+      this.isShow = false
+      this.$emit('cancel')
+      this.$emit('close')
+    },
+  },
+}
+</script>
+<style lang="less" scope>
+.message {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,27 @@
-import Modal from './Modal.vue'
 import Overlay from './Overlay.vue'
+import Modal from './Modal.vue'
+import Alert from './Alert.vue'
 import CountDown from './CountDown.vue'
 
 export default {
   install(Vue, options) {
-    Vue.component('Modal', Modal)
     Vue.component('Overlay', Overlay)
+    Vue.component('Modal', Modal)
+    Vue.component('Alert', Alert)
     Vue.component('CountDown', CountDown)
+
+    Vue.prototype.$alert = function(message, cb) {
+      var alert = new Vue(Alert)
+      alert.isShow = false
+      alert.message = message
+      alert.$on('close', cb)
+      alert.$nextTick(e => {
+        alert.isShow = true
+      })
+
+      var el = document.createElement('div')
+      document.body.appendChild(el)
+      alert.$mount(el)
+    }
   },
 }
